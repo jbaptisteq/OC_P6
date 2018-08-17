da0514c5ff9967538740cbb644dc7b233e41148c
jbaptiste <jb.queralt@gmail.com>
2018-08-17 16:43:33 +0200

    divers : Adding working files

--- src/p6/UserBundle/Controller/SecurityController.php
+++ src/p6/UserBundle/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use p6\UserBundle\Entity\User;
 
 
 class SecurityController extends Controller
@@ -16,7 +17,7 @@ class SecurityController extends Controller
     public function loginAction()
     {
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')){
-            return $this->redirectToRoute('p6_core_homepage');
+            return $this->redirectToRoute('homepage');
         }
 
         $authenticationUtils = $this->get('security.authentication_utils');
@@ -31,7 +32,15 @@ class SecurityController extends Controller
      * @Route("/forgot_password", name="forget")
      */
     public function forgetAction()
-    {   
+    {
         return $this->render('p6UserBundle:Security:forget.html.twig');
+    }
+
+    /**
+     * @Route("/reset_password", name="reset")
+     */
+    public function resetAction()
+    {
+        return $this->render('p6UserBundle:Security:reset.html.twig');
     }
 }
