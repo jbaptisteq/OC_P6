d7b8cb548ad52c2b520f0e39889807a02cb6c4e9
jbaptiste <jb.queralt@gmail.com>
2018-09-12 11:59:59 +0200

    refactor : avatar entity

--- src/p6/UserBundle/Entity/avatar.php
+++ src/p6/UserBundle/Entity/avatar.php
@@ -5,92 +5,60 @@ namespace p6\UserBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Avatar
- *
- * @ORM\Table(name="avatar")
- * @ORM\Entity(repositoryClass="p6\UserBundle\Repository\avatarRepository")
- */
+* Avatar
+*
+* @ORM\Table(name="avatar")
+* @ORM\Entity(repositoryClass="p6\UserBundle\Repository\avatarRepository")
+*/
 class Avatar
 {
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+  /**
+  * @var int
+  *
+  * @ORM\Column(name="id", type="integer")
+  * @ORM\Id
+  * @ORM\GeneratedValue(strategy="AUTO")
+  */
+  private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="url", type="string", length=255)
-     */
-    private $url;
+  /**
+  * @var string
+  *
+  * @ORM\Column(name="url", type="string", length=255)
+  */
+  private $url;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="alt", type="string", length=255)
-     */
-    private $alt;
+  /**
+  * Get id.
+  *
+  * @return int
+  */
+  public function getId()
+  {
+    return $this->id;
+  }
 
+  /**
+  * Set url.
+  *
+  * @param string $url
+  *
+  * @return avatar
+  */
+  public function setUrl($url)
+  {
+    $this->url = $url;
 
-    /**
-     * Get id.
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+    return $this;
+  }
 
-    /**
-     * Set url.
-     *
-     * @param string $url
-     *
-     * @return avatar
-     */
-    public function setUrl($url)
-    {
-        $this->url = $url;
-
-        return $this;
-    }
-
-    /**
-     * Get url.
-     *
-     * @return string
-     */
-    public function getUrl()
-    {
-        return $this->url;
-    }
-
-    /**
-     * Set alt.
-     *
-     * @param string $alt
-     *
-     * @return avatar
-     */
-    public function setAlt($alt)
-    {
-        $this->alt = $alt;
-
-        return $this;
-    }
-
-    /**
-     * Get alt.
-     *
-     * @return string
-     */
-    public function getAlt()
-    {
-        return $this->alt;
-    }
+  /**
+  * Get url.
+  *
+  * @return string
+  */
+  public function getUrl()
+  {
+    return $this->url;
+  }
 }
