24cede3b7612810615cce25ec139372856eae27e
jbaptiste <jb.queralt@gmail.com>
2018-09-14 20:45:57 +0200

    Branch for Phpu!nit Test

---
+++ src/p6/CoreBundle/Tests/Controller/IndexControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace p6\CoreBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use p6\CoreBundle\Controller;
+
+class IndexControllerTest extends WebTestCase
+{
+  public function testIndexAction()
+  {
+
+  }
+}
